822365d7dc4aa4b667d1e27e4dfe7afa16b8dfa2
FilippoBonanno <fbonanno15@gmail.com>
2024-04-27 13:28:28 +0200

    M-3

--- js/main.js
+++ js/main.js
@@ -4,29 +4,32 @@ console.log(slides);
 let imgActive;
 let nextSlide;
 
-// CREO UN CICLO CHE AUMENTI DI 1 E CHE FINISCA QUANDO ARRIVA A < DI 5 IN QUESTO CASO
-for (let i = 0; i < slides.length; i++) {
-    const slide = slides[i];
-    console.log(i);
+// INSERISCO UN EVENTLISTNER CHE SOLO AL CLICK MI INCLUDA TUTTO QUELLO CHE AVEVO INSERITO
+document.getElementById("next").addEventListener("click", function () {
+    // CREO UN CICLO CHE AUMENTI DI 1 E CHE FINISCA QUANDO ARRIVA A < DI 5 IN QUESTO CASO
+    for (let i = 0; i < slides.length; i++) {
+        const slide = slides[i];
+        console.log(i);
 
-    // SE CONTIENE LA CLASSE ACTIVE GLIELA TOLGO
-    if (slide.classList.contains("active")) {
-        //se si, rimuovo la classe active
-        slide.classList.remove("active");
-        //e prendo nota dell'indice
-        imgActive = i;
-        console.log("Ho rimosso la classe active dall'elemento: ", i);
+        // SE CONTIENE LA CLASSE ACTIVE GLIELA TOLGO
+        if (slide.classList.contains("active")) {
+            //se si, rimuovo la classe active
+            slide.classList.remove("active");
+            //e prendo nota dell'indice
+            imgActive = i;
+            console.log("Ho rimosso la classe active dall'elemento: ", i);
+        }
     }
-}
 
-// ADESSO DICO CHE LA SLIDE SUCCESSIVA è QUELLA ATTIVA + 1
-nextSlide = imgActive + 1;
-console.log (nextSlide);
+    // ADESSO DICO CHE LA SLIDE SUCCESSIVA è QUELLA ATTIVA + 1
+    nextSlide = imgActive + 1;
+    console.log (nextSlide);
 
-if (nextSlide >= slides.length) {
-    nextSlide = 0;
-}
- 
-// METTO ACTIVE ALLA SLIDE SUCCESSIVA
-slides[nextSlide].classList.add("active");
-console.log (`classe attiva: ${nextSlide}`);
+    if (nextSlide >= slides.length) {
+        nextSlide = 0;
+    }
+    
+    // METTO ACTIVE ALLA SLIDE SUCCESSIVA
+    slides[nextSlide].classList.add("active");
+    console.log (`classe attiva: ${nextSlide}`);
+});
