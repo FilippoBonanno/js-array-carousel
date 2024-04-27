e57b4e6101f93e74d4972843c66fccd657f71daf
FilippoBonanno <fbonanno15@gmail.com>
2024-04-27 13:25:35 +0200

    M-2

---
+++ js/main.js
@@ -0,0 +1,32 @@
+// Recupero tutti gli elementi necessari da gestire
+let slides = document.getElementsByClassName("slide");
+console.log(slides);
+let imgActive;
+let nextSlide;
+
+// CREO UN CICLO CHE AUMENTI DI 1 E CHE FINISCA QUANDO ARRIVA A < DI 5 IN QUESTO CASO
+for (let i = 0; i < slides.length; i++) {
+    const slide = slides[i];
+    console.log(i);
+
+    // SE CONTIENE LA CLASSE ACTIVE GLIELA TOLGO
+    if (slide.classList.contains("active")) {
+        //se si, rimuovo la classe active
+        slide.classList.remove("active");
+        //e prendo nota dell'indice
+        imgActive = i;
+        console.log("Ho rimosso la classe active dall'elemento: ", i);
+    }
+}
+
+// ADESSO DICO CHE LA SLIDE SUCCESSIVA è QUELLA ATTIVA + 1
+nextSlide = imgActive + 1;
+console.log (nextSlide);
+
+if (nextSlide >= slides.length) {
+    nextSlide = 0;
+}
+ 
+// METTO ACTIVE ALLA SLIDE SUCCESSIVA
+slides[nextSlide].classList.add("active");
+console.log (`classe attiva: ${nextSlide}`);
